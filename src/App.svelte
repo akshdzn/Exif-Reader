43b610c4f361321c2f4e2d9d0ee2305880147869
akshdzn <222266665+akshdzn@users.noreply.github.com>
2026-01-18 10:46:26 +0530

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+  <script>
+    import { getAllTags } from 'exif-js';
+    import ExifReader, { errors } from 'exifreader';
+
+    let imgInput;
+    let exifData = null;
+
+    $: if (imgInput && imgInput.length > 0) {
+      const img = imgInput[0];
+      ExifReader.load(img).then((tags)=>{exifData = tags});
+    }
+  </script>
+
+  <main>
+    <input type="file" id="imageInput" accept="image/*" bind:files={imgInput}>
+    {#if exifData}
+      <pre>{JSON.stringify(exifData)}</pre>
+    {/if}
+  </main>
