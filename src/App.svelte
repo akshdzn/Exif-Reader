cad25461ec0af41566937f38f8fc582258dc3345
akshdzn <222266665+akshdzn@users.noreply.github.com>
2026-01-18 17:53:21 +0530

    base app

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,74 @@
-  <script>
-    import { getAllTags } from 'exif-js';
-    import ExifReader, { errors } from 'exifreader';
+<script>
+    import { getAllTags } from "exif-js";
+    import ExifReader, { errors } from "exifreader";
 
+    import { UploadSimple, FileImage } from "phosphor-svelte";
+
+    let inputBox;
     let imgInput;
     let exifData = null;
 
-    $: if (imgInput && imgInput.length > 0) {
-      const img = imgInput[0];
-      ExifReader.load(img).then((tags)=>{exifData = tags});
-    }
-  </script>
+    $: imgSrc =
+        imgInput && imgInput.length > 0
+            ? URL.createObjectURL(imgInput[0])
+            : null;
 
-  <main>
-    <input type="file" id="imageInput" accept="image/*" bind:files={imgInput}>
-    {#if exifData}
-      <pre>{JSON.stringify(exifData)}</pre>
-    {/if}
-  </main>
+    $: if (imgInput && imgInput.length > 0) {
+        const img = imgInput[0];
+        ExifReader.load(img).then((tags) => {
+            exifData = tags;
+        });
+    }
+</script>
+
+<main>
+    <div class="title-bx">
+        <div class="title-border">EXIF Reader</div>
+        <div class="title">EXIF Reader</div>
+    </div>
+
+    <input
+        bind:this={inputBox}
+        type="file"
+        id="imageInput"
+        accept="image/*"
+        bind:files={imgInput}
+        class="fileInput"
+    />
+    <button
+        class="fileInputButton"
+        on:click={() => {
+            inputBox.click();
+        }}
+    >
+        <UploadSimple size={24}></UploadSimple>
+        Upload image
+    </button>
+
+    <button
+        class="image-viewer"
+        on:click={() => {
+            inputBox.click();
+        }}
+    >
+        {#if imgSrc}
+            <img src={imgSrc} alt="img" class="img" />
+        {:else}
+            <div class="image-viewer-alert">
+                <FileImage size={43}></FileImage>
+                Upload an image to view its metadata
+            </div>
+        {/if}
+    </button>
+
+    <div class="list-bx">
+        {#if exifData}
+            {#each Object.entries(exifData) as key}
+                <div class="list-item">
+                    <div class="list-item-name">{key[0]}</div>
+                    <div class="list-item-value">{key[1].value}</div>
+                </div>
+            {/each}
+        {/if}
+    </div>
+</main>
